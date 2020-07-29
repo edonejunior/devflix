fc2037e735713a303eb00beeca8257d09a0448c2
Edvaldo Domingues de Faria Junior <edonejunior@hotmail.com>
2020-07-29 12:36:48 -0300

    form imputs

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,27 +1,87 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PageDefault from '../../../components/PageDefault';
 import {Link} from 'react-router-dom';
 
+
+
 function CadastroCategoria() {
+
+  const [categorias, setCategorias] = useState([]);
+  const [nomeDaCategoria, setNomeDaCategoria] = useState('Categoria Inicial');
+
+
   return (
      
        <PageDefault >
-        <h1>Cadastro de Categoria</h1>
+        <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
 
-        <form>
+        <form onSubmit={ function handleSubmit(event) {
+          event.preventDefault();
+          setCategorias([
+            ...categorias, 
+            nomeDaCategoria
+          ]);
+
+        } }>
+          <div>
 
             <label>
               Nome da Categoria:
               <input
                 type="text"
+                value={nomeDaCategoria}
+                onChange={function HandlerImput(info) {
+                  setNomeDaCategoria(info.target.value)
+                }}
+
               />
             </label>
+          </div>
+          <div>
+
+            <label>
+              Descrição:
+              <textarea
+                type="text"
+                value={nomeDaCategoria}
+                onChange={function HandlerImput(info) {
+                  setNomeDaCategoria(info.target.value)
+                }}
+
+              />
+            </label>
+          </div>
+          
+          <div>
+
+            <label>
+              Cor:
+              <input
+                type="color"
+                value={nomeDaCategoria}
+                onChange={function HandlerImput(info) {
+                  setNomeDaCategoria(info.target.value)
+                }}
+
+              />
+            </label>
+          </div>
 
             <button>
               Cadastrar
             </button>
             
         </form>
+
+        <ul>
+          {categorias.map((categoria, indice) => {
+            return (
+              <li key={`${categoria}${indice}`}>
+                {categoria}
+              </li>
+            )
+          })}
+        </ul>
 
         <Link to="/">
           Ir para a Home
