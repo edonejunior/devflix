251a47533aa9d0d4825ae1880dc210af9ca31b55
Edvaldo Domingues de Faria Junior <edonejunior@hotmail.com>
2020-07-29 09:39:49 -0300

    PageDefault_Categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import PageDefault from '../../../components/PageDefault';
 import {Link} from 'react-router-dom';
 
-/* import { Container } from './styles'; */
-
-function CadastroVideo() {
+function CadastroCategoria() {
   return (
      
        <PageDefault >
-        <h1>Cadastro de vídeo</h1>
+        <h1>Cadastro de Categoria</h1>
 
-        <Link to="/cadastro/categoria">
-          Cadastrar Categoria
+        <Link to="/">
+          Ir para a Home
         </Link>
 
        </PageDefault>
@@ -19,4 +17,4 @@ function CadastroVideo() {
   );
 }
 
-export default CadastroVideo;
+export default CadastroCategoria;
